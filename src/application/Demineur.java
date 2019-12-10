84669ff87784364c4bc0140cf12772ab445636a6
MrAnyx <1ptitigre@gmail.com>
2019-12-10 18:09:39 +0100

    [add] final démineuré

--- src/application/Demineur.java
+++ src/application/Demineur.java
@@ -10,18 +10,114 @@ import javafx.scene.image.Image;
 
 public class Demineur {
 	
-	private Case[][] jeu;
-	private boolean playerWin;
+	private Case[][] jeu = new Case[16][16];
+	private boolean playerLose;
 	private int getNbMineSuspecte;
 	
 	Demineur(){
+		
+		//initialisation des cases
 		for(int i = 0; i<16; i++) {
 			for(int j = 0; j<16; j++){
-				this.jeu[i][j] = new Case(false,false,false,0);
+				this.jeu[i][j] = new Case(false, false, false, 0);
 			}
 		}
-		this.playerWin = false;
+		// initialisation des mines
+		for(int j = 0; j<16; j++) {
+			for(int k = 0; k<1; k++) {
+				this.jeu[(int)Math.ceil(Math.random()*15)][j].setMine(true);
+			}
+		}
+	
+		
+		//nb Mine around
+		for(int i = 0; i<16; i++) {
+			for(int j = 0; j<16; j++){
+				this.jeu[i][j].setNbMineAround(nbMineAround(i, j));
+			}
+		}
+		
+		
+		this.playerLose = false;
 		this.getNbMineSuspecte = 0;
+	}
+	
+	private int nbMineAround(int i, int j) {
+		if(!this.jeu[i][j].isMine() && i>0 && i<15 && j<15 && j>0) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j+1].isMine())+boolToInt(this.jeu[i+1][j-1].isMine())+boolToInt(this.jeu[i][j+1].isMine())+boolToInt(this.jeu[i][j-1].isMine())+boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j-1].isMine())+boolToInt(this.jeu[i-1][j+1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==0 && j<15 && j>0) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j+1].isMine())+boolToInt(this.jeu[i+1][j-1].isMine())+boolToInt(this.jeu[i][j+1].isMine())+boolToInt(this.jeu[i][j-1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==15 && j<15 && j>0) {
+			return boolToInt(this.jeu[i][j+1].isMine())+boolToInt(this.jeu[i][j-1].isMine())+boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j+1].isMine())+boolToInt(this.jeu[i-1][j-1].isMine());
+		}if(!this.jeu[i][j].isMine() && i>0 && i<15 && j==0) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j+1].isMine())+boolToInt(this.jeu[i][j+1].isMine())+boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j+1].isMine());
+		}if(!this.jeu[i][j].isMine() && i>0 && i<15 && j==15) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j-1].isMine())+boolToInt(this.jeu[i][j-1].isMine())+boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j-1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==0 && j==0) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j+1].isMine())+boolToInt(this.jeu[i][j+1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==0 && j==15) {
+			return boolToInt(this.jeu[i+1][j].isMine())+boolToInt(this.jeu[i+1][j-1].isMine())+boolToInt(this.jeu[i][j-1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==15 && j==0) {
+			return boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j+1].isMine())+boolToInt(this.jeu[i][j+1].isMine());
+		}if(!this.jeu[i][j].isMine() && i==15 && j==15) {
+			return boolToInt(this.jeu[i-1][j].isMine())+boolToInt(this.jeu[i-1][j-1].isMine())+boolToInt(this.jeu[i][j-1].isMine());
+		}else {
+			return 1;
+		}
+		
+		
+	}
+	
+	public void expansion(int i, int j) {
+		this.jeu[i][j].setVisible(true);
+
+		
+		if(j<15 && !this.jeu[i][j+1].isVisible()) {
+			this.jeu[i][j+1].setVisible(true);
+			if(this.jeu[i][j+1].getNbMineAround() == 0 ){
+				expansion(i, j+1);
+			}
+		}if(j>0 && !this.jeu[i][j-1].isVisible()) {
+			this.jeu[i][j-1].setVisible(true);
+			if(this.jeu[i][j-1].getNbMineAround() == 0 ) {
+				expansion(i, j-1);
+			}
+		}if(i<15 && !this.jeu[i+1][j].isVisible()) {
+			this.jeu[i+1][j].setVisible(true);
+			if(this.jeu[i+1][j].getNbMineAround() == 0) {
+				expansion(i+1, j);
+			}
+		}if(i<15 && j<15 && !this.jeu[i+1][j+1].isVisible()) {
+			this.jeu[i+1][j+1].setVisible(true);
+			if(this.jeu[i+1][j+1].getNbMineAround() == 0) {
+				expansion(i+1, j+1);
+			}
+		}if(i<15 && j>0 && !this.jeu[i+1][j-1].isVisible()) {
+			this.jeu[i+1][j-1].setVisible(true);
+			if(this.jeu[i+1][j-1].getNbMineAround() == 0) {
+				expansion(i+1, j-1);
+			}
+		}if(i>0 && j<15 && !this.jeu[i-1][j+1].isVisible()) {
+			this.jeu[i-1][j+1].setVisible(true);
+			if(this.jeu[i-1][j+1].getNbMineAround() == 0) {
+				expansion(i-1, j+1);
+			}
+		}if(i>0 && j>0 && !this.jeu[i-1][j-1].isVisible()) {
+			this.jeu[i-1][j-1].setVisible(true);
+			if(this.jeu[i-1][j-1].getNbMineAround() == 0) {
+				expansion(i-1, j-1);
+			}
+		}if(i>0 && !this.jeu[i-1][j].isVisible()) {
+			this.jeu[i-1][j].setVisible(true);
+			if(this.jeu[i-1][j].getNbMineAround() == 0) {
+				expansion(i-1, j);
+			}
+		}
+		
+	}
+	
+	private int boolToInt(boolean b) {
+		return b ? 1 : 0;
 	}
 	
 	public Map<String, Image> getTabImage() {
@@ -41,6 +137,46 @@ public class Demineur {
 		}  
 		return images;
 	}
+
+	public Case[][] getJeu() {
+		return this.jeu;
+	}
+
+	public void setJeu(Case[][] jeu) {
+		this.jeu = jeu;
+	}
+
+	
+	public boolean isPlayerLose() {
+		return this.playerLose;
+	}
+
+	public void setPlayerLose(boolean playerLose) {
+		this.playerLose = playerLose;
+	}
+
+	public int getGetNbMineSuspecte() {
+		return this.getNbMineSuspecte;
+	}
+
+	public void setGetNbMineSuspecte(int getNbMineSuspecte) {
+		this.getNbMineSuspecte = getNbMineSuspecte;
+	}
+	
+	public boolean playerWin() {
+		
+		
+		for(int i = 0; i<16; i++) {
+			for(int j = 0; j<16; j++) {
+				if(!this.jeu[i][j].isVisible() && !this.jeu[i][j].isMine()) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 	
 
 
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,38 +2,118 @@ package application;
 
 import java.util.Map;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 public class MainController {
 	
 	@FXML Canvas canvas;
 	
+	@FXML TextField tfTimer;
+	
+	@FXML Timeline tm;
+	
+	@FXML Button btnRestart;
+	
+	@FXML TextField tfNbMines;
+	
+	private Demineur game;
+	private Map<String, Image> images;
+	private GraphicsContext gc;
+	private int currentTime = 0;
+	
+	private int tmp;
+	
 	public void initialize() {
-		Demineur game = new Demineur();
+		game = new Demineur();
 		
-		Map<String, Image> images = game.getTabImage();
+		images = game.getTabImage();
 		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc = canvas.getGraphicsContext2D();
 		
+		actualiser();
 		
-		gc.drawImage(images.get("bombe"), 32*0, 0);
-		gc.drawImage(images.get("tuile"), 32*1, 0);
-		gc.drawImage(images.get("tuile1"), 32*2, 0);
-		gc.drawImage(images.get("tuile2"), 32*3, 0);
-		gc.drawImage(images.get("tuile3"), 32*4, 0);
-		gc.drawImage(images.get("tuile4"), 32*5, 0);
-		gc.drawImage(images.get("tuile5"), 32*6, 0);
-		gc.drawImage(images.get("tuile6"), 32*7, 0);
-		gc.drawImage(images.get("tuile7"), 32*8, 0);
-		gc.drawImage(images.get("tuile8"), 32*9, 0);
-		gc.drawImage(images.get("tuile9"), 32*10, 0);
-		gc.drawImage(images.get("suspecte"), 32*11, 0);
+		tm = new Timeline(new KeyFrame(Duration.millis(10), e -> {
+			tmp++;
+			if(tmp % 100 == 0) {
+				this.currentTime++;
+				tfTimer.setText(String.format("%03d", this.currentTime));
+			}
+		}));
+		tm.setCycleCount(Timeline.INDEFINITE);
+		tm.play();
+	}
+	
+	@FXML public void actionReset(){
+		game = new Demineur();
+		tm.play();
+		this.currentTime = 0;
+		actualiser();
+	}
+	
+	@FXML private void actionClic(MouseEvent evt) {
+		int clicX = (int) evt.getX();
+		int clicY = (int) evt.getY();
+		MouseButton bt = evt.getButton();
 		
-		
-		
+		if(bt == MouseButton.PRIMARY && !game.isPlayerLose()) {
+			if(game.playerWin()) {
+				btnRestart.setText("Player Win");
+			}else {
+				if(game.getJeu()[clicX/32][clicY/32].isMine()) {
+					game.setPlayerLose(true);
+					tm.stop();
+				}
+				if(game.getJeu()[clicX/32][clicY/32].getNbMineAround() == 0) {
+					game.expansion(clicX/32, clicY/32);
+				}else {
+					game.getJeu()[clicX/32][clicY/32].setVisible(true);
+				}
+				actualiser();
+			}
+		}else if(bt == MouseButton.SECONDARY && !game.isPlayerLose()) {
+			if(game.playerWin()) {
+				btnRestart.setText("Player Win");
+			}else {
+				if(!game.getJeu()[clicX/32][clicY/32].isVisible()) {
+					if(game.getJeu()[clicX/32][clicY/32].isMineForPlayer()) {
+						game.getJeu()[clicX/32][clicY/32].setMineForPlayer(false);
+						game.setGetNbMineSuspecte(game.getGetNbMineSuspecte()-1);
+						tfNbMines.setText(String.format("%02d", game.getGetNbMineSuspecte()));
+					}else {
+						game.getJeu()[clicX/32][clicY/32].setMineForPlayer(true);
+						game.setGetNbMineSuspecte(game.getGetNbMineSuspecte()+1);
+						tfNbMines.setText(String.format("%02d", game.getGetNbMineSuspecte()));
+					}
+					actualiser();
+				}
+			}
+		}
+	}
+	
+	private void actualiser() {
+		for(int i = 0; i<16; i++) {
+			for(int j = 0; j<16; j++) {
+				if(!game.getJeu()[i][j].isVisible()) {
+					if(!game.getJeu()[i][j].isMineForPlayer()) gc.drawImage(images.get("tuile"), i*32, j*32);
+					else gc.drawImage(images.get("suspecte"), i*32, j*32);
+				}else {
+					if(game.getJeu()[i][j].isMine()) gc.drawImage(images.get("bombe"), i*32, j*32);
+					else {
+						gc.drawImage(images.get(String.format("tuile%d", game.getJeu()[i][j].getNbMineAround())), i*32, j*32);
+					}
+				}
+			}
+		}
 	}
 	
 }
